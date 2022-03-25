4ec3e330311bdf8305106bded0bac7f4728001b0
swefd <swefday@gmail.com>
2022-03-25 15:48:09 +0200

    v3

--- download.py
+++ download.py
@@ -3,24 +3,32 @@ import ssl
 import os
 import sys
 
+ssl._create_default_https_context = ssl._create_unverified_context
+
 def bar_progress(current, total, width=80):
     progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
     # Don't use print() as it will print in new line every time.
     sys.stdout.write("\r" + progress_message)
     sys.stdout.flush()
 
-
+# Create two dirs
 if not os.path.exists('res'):
     os.makedirs('res', exist_ok=False)
-os.chdir('res')
-
-
 
 if not os.path.exists('yolo'):
     os.makedirs('yolo', exist_ok=False)
-os.chdir('yolo')
 
-ssl._create_default_https_context = ssl._create_unverified_context
+# Go to res
+os.chdir('res')
+
+# Pizza
+wget.download('https://raw.githubusercontent.com/swefd/YOLOV3_AI_L1/master/res/pizza.jpeg', bar=bar_progress)
+
+# Plane
+wget.download('https://raw.githubusercontent.com/swefd/YOLOV3_AI_L1/master/res/plane.jpeg', bar=bar_progress)
+
+# Go to yolo
+os.chdir('yolo')
 
 # Names
 wget.download('https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names', bar=bar_progress)
